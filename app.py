de7c0347ddf36474d718506c754e69ee0b68e8d9
Controldegestion <cenoacontrol@gmail.com>
2025-07-10 17:17:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def validate_file_date(filename):
         two_months_ago = current_month - 2 if current_month > 2 else 12 + (current_month - 2)
         two_months_ago_year = current_year if current_month > 2 else current_year - 1
         # Configuracion Ajuste
-        if (file_date.year == two_months_ago_year and file_date.month == two_months_ago and now.day <= 10):
+        if (file_date.year == two_months_ago_year and file_date.month == two_months_ago and now.day <= 12):
             return True
 
         return False
